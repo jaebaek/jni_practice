834f2fba813e32baf8bb1ab0f5aa97b009e979f9
jaebaek <jaebaek@cps.kaist.ac.kr>
2014-08-12 13:42:07 +0900

    Fix HelloWorld bug

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,9 +1,9 @@
-public class HelloJNI {
+public class HelloWorld {
     static {
         System.loadLibrary("hello");
     }
     private native void sayHello();
     public static void main (String [] args) {
-        new HelloJNI().sayHello();
+        new HelloWorld().sayHello();
     }
 }
